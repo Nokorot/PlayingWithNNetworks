978ad2bb8f81412682200c79fa92d9b8c862947f
HealyTea12 <tarikcavalcanti12@gmail.com>
2021-11-25 19:28:04 +0100

    committed update

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
 import numpy as np
 #import matplotlib.pyplot as plt
-import neuralNetwork
+import neuralNetwork as nn
+import datahandler as dh
 
-def readImg (filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    n_of_imgs = int.from_bytes(data[4:8], "big")
-    n_of_rows = int.from_bytes(data[8:12], "big")
-    n_of_cols = int.from_bytes(data[12:16], "big")
 
-    images = numpy.zeros((n_of_imgs,n_of_rows, n_of_cols), dtype=numpy.uint8)
-
-    k = 0
-    for i in data[16:]:
-
-        imgi = k//(n_of_rows*n_of_cols)
-        rowi = (k - imgi*n_of_rows*n_of_cols)//n_of_cols
-        coli = (k - imgi*n_of_rows*n_of_cols-rowi*n_of_cols)
-        images[imgi,rowi,coli] = i
-        k+=1
-        if k%100000 == 0:
-            print(k)
-
-    numpy.save("images",images)
-
-def readLabels (filename):
-    f = open(filename, "rb")
-    labels = f.read()
-    labels = numpy.frombuffer(labels, dtype=numpy.uint8)
-    labels = numpy.delete(labels, [0,1,2,3,4,5,6,7])
-    numpy.save("labels",labels)
 
 def main():
-    network = neuralNetwork.Network([28*28,16,16,10])
-    print(np.shape(network.weights[0]))
+    images, labels = dh.load_training_data()
+
+    network = nn.Network([28*28,16,16,10])
+    #print(network.costData(image[0].flatten()))
+    print(network.costData(images.flatten(),labels))
 main()
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -10,21 +10,21 @@ class Network:
         self.weights = [np.random.rand(x,y) for x, y in zip(type[1:], type[:-1])]
 
     def forward(self, input):
-        input = input.reshape(1,len(input))
-        for i in range(self.n_layers-1):
-            for weight,bias in zip(self.weights, self.biases):
-                input = self.sigmoid(weight@input+b)
+        input = input.reshape(len(input),1)
+        for weight,bias in zip(self.weights, self.biases):
+            input = self.sigmoid(weight@input+bias)
         return input
 
     def costLabel(self, input, label):
-        output = forward(input)
-        return np.dot(output-label,output-label)
+        output = self.forward(input)
+        label = np.reshape(len(input),1)
+        return np.dot(np.transpose(output-label),output-label)
 
     #returns average cost given a set of data
     def costData(self, inputs, labels):
         totalCost=0
         for input, label in zip(inputs, labels):
-            totalCost+=costLabel(input, label)
+            totalCost+=self.costLabel(input, label)
         return totalCost/len(inputs)
 
     #backprop for 1 input-label
@@ -43,10 +43,11 @@ class Network:
             zs.append(input)
             layers.append(sigmoid(input))
 
-        gradw[0][j,k] = 2*(layers[-1]-label)*sigmoidprime(zs[-1])@np.transpose(layers[-2])
-        gradb[0] = 2*(layers[-1]-label)*sigmoidprime(zs[-1])
+        gradw[0] += 2*(layers[-1]-label)*sigmoidprime(zs[-1])@np.transpose(layers[-2])
+        gradb[0] += 2*(layers[-1]-label)*sigmoidprime(zs[-1])
 
-            """2(aj-yj)sigmaprime(z)*ak=delwjk=dC/dwjk"""
+        """2(aj-yj)sigmaprime(z)*ak=delwjk=dC/dwjk"""
+
 
 
     def sigmoid(self, argument):
