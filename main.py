00c03189c45f61bcc5d5a8a6a74d6dbaf0158872
HealyTea12 <tarikcavalcanti12@gmail.com>
2021-11-25 15:07:48 +0100

    Introduced Network class

--- main.py
+++ main.py
@@ -23,7 +23,48 @@ def readImg (filename):
 
     numpy.save("images",images)
 
+def readLabels (filename):
+    f = open(filename, "rb")
+    labels = f.read()
+    labels = numpy.frombuffer(labels, dtype=numpy.uint8)
+    labels = numpy.delete(labels, [0,1,2,3,4,5,6,7])
+    numpy.save("labels",labels)
+
+def cost(result, label):
+    np.dot(result - label,result-label)
+
+def transformm(vector, weights, biases):
+    x = weights * vector + biases
+    x = 1/(1+np.exp(-x))
+
 #readImg("data/train-images-idx3-ubyte")
-data=numpy.load("images.npy")
-plt.imshow(data[0])
-plt.show()
+#readLabels("data/train-labels-idx1-ubyte")
+data = numpy.load("images.npy")
+labels = numpy.load("labels.npy")
+
+imgsize=len(data[0].flatten())
+
+weights1 = np.random.rand((16,imgsize))
+weights2 = np.random.rand(16,16)
+weights3 = np.random.rand(10,16)
+biases1 = np.random.rand(16)
+biases2 = np.random.rand(16)
+biases3 = np.random.rand(10)
+
+img = images[0].flatten()
+def adapt(lbl):
+    image = img.flatten()
+    label = numpy.zeros(10)
+    for i in range(0, 10):
+        if labels[0] == i:
+            label[i] = 1
+    return label
+
+def actualCost(img, label):
+    img = transformm(img, weights1, biases1)
+    img = transformm(img, weights2, biases2)
+    img = transformm(img, weights3, biases3)
+    cost = cost(img, label)
+
+#plt.imshow(data[0])
+#plt.show()
---
+++ neuralNetwork.py
@@ -0,0 +1,43 @@
+import numpy as np
+import random
+
+class Network:
+    def __init__(self, type):
+        #type list with number of nodes per layer
+        self.type = type
+        self.n_layers = len(type)
+        self.biases = [np.random.rand(x) for x in type[1:]]
+        self.weights = [np.random.rand(x,y) for x, y in zip(type[1:], type[:-1]))]
+
+    def forward(self, input):
+        for i in range(self.n_layers-1):
+            for weight,bias in zip(self.weights, self.biases):
+                input = self.sigmoid(weight@input+b)
+        return input
+
+    def costLabel(self, input, label):
+        output = forward(input)
+        return np.dot(output-label,output-label)
+
+    #returns average cost given a set of data
+    def costData(self, inputs, labels):
+        totalCost=0
+        for input, label in zip(inputs, labels):
+            totalCost+=costLabel(input, label)
+        return totalCost/len(inputs)
+
+    #backprop for 1 input-label
+    def backprop(self, input, label):
+        #list of grads in the for of weights and biases from self:
+        gradw = [np.zeros(np.shape(weight)) for weight in self.weights]
+        gradb = [np.zeros(np.shape(bias)) for bias in self.biases]
+        #do the forward propagation, saving everything:
+        layers = [input]
+        zs = []
+        for weight, bias in zip(self.weights, self.biases):
+
+
+    def sigmoid(self, argument):
+        return 1/(1+np.exp(-argument))
+    def sigmoidprime(self, argument):
+        exp(-argument)/(1+np.exp(-argument))**2
