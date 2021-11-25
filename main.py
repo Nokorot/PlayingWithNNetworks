8bd29b09e28c894776801f2eba4f82180a7b8dbc
Tor-Haakon <torhaakon2012@hotmail.com>
2021-11-25 19:47:45 +0100

    Flattening images

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import numpy as np
-#import matplotlib.pyplot as plt
 import neuralNetwork as nn
 import datahandler as dh
+
 
 
 
 def main():
     images, labels = dh.load_training_data()
 
+    ni, w, h = images.shape
+    images = images.reshape(ni,w*h)
+    
+
     network = nn.Network([28*28,16,16,10])
-    #print(network.costData(image[0].flatten()))
-    print(network.costData(images.flatten(),labels))
+
+    print(network.costLabel(images[0],labels[0]))
+    print(network.costData(images,labels))
+
 main()
